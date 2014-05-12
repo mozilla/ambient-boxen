1c1715f47e4a48c9da8e12836e9b83a375f442d2
lonnen <chris.lonnen@gmail.com>
2014-05-12 15:28:01 -0700

    remove resource alias in favor of the File type

--- modules/ambient_mozilla/manifests/init.pp
+++ modules/ambient_mozilla/manifests/init.pp
@@ -4,15 +4,15 @@ class ambient_mozilla{
 
   file { "${boxen::config::home}/profile":
     ensure => 'directory',
-    alias  => 'copy_profile',
     source => "${boxen::config::repodir}/modules/ambient_mozilla/files/profile",
   }
 
   exec { 'start_firefox':
-    command =>
+    command   =>
     "/Applications/FirefoxAurora.app/Contents/MacOSX/firefox
       -profile ${boxen::config::home}/profile",
-    require => 'copy_profile',
+    subscribe => File["${boxen::config::home}/profile"]
+,
   }
 
 }
