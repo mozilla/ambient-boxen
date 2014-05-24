9c348aed6c18c51d6fb07eefaece5c35289f6ef1
lonnen <chris.lonnen@gmail.com>
2014-05-23 18:21:12 -0700

    stop checking for default browser on startup

--- modules/ambient_mozilla/files/FirefoxPrefs.js
+++ modules/ambient_mozilla/files/FirefoxPrefs.js
@@ -3,6 +3,7 @@
 user_pref("app.update.auto", "false");
 user_pref("app.update.enabled", "false");
 user_pref("browser.showedAustralisWhatsNew", "true");
-user_pref("browser.startup.homepage", "https://airmozilla-ops2.corpdmz.scl3.mozilla.com/");
+user_pref("browser.startup.homepage", "http://airmozilla-ops2.corpdmz.scl3.mozilla.com/");
 user_pref("full-screen-api.allow-trusted-requests-only", "false");
 user_pref("full-screen-api.approval-required", "false");
+user_pref("browser.shell.checkDefaultBrowser", "false");
