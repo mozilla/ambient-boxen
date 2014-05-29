ff9debd9c94efaa09c1ec5b6176253f2333eb005
lonnen <chris.lonnen@gmail.com>
2014-05-29 10:45:28 -0700

    add OSX and iterm2 includes, move user.js to the new firefox_profile directory

--- modules/ambient_mozilla/manifests/init.pp
+++ modules/ambient_mozilla/manifests/init.pp
@@ -1,6 +1,9 @@
 # Public: set up a machine to be an ambient mozilla display
 class ambient_mozilla{
   include firefox::aurora
+  include iterm2
+
+  include osx::software_update
 
   exec { "create_firefox_profile":
     command => "/Applications/FirefoxAurora.app/Contents/MacOS/firefox -CreateProfile \"ambient_mozilla ${boxen::config::home}/firefox_profile\"",
@@ -8,7 +11,7 @@ class ambient_mozilla{
     require => Package['Firefox-Aurora']
   }
 
-  file { "${boxen::config::home}/profile/user.js":
+  file { "${boxen::config::home}/firefox_profile/user.js":
     source  => "${boxen::config::repodir}/modules/ambient_mozilla/files/FirefoxPrefs.js",
     require => Exec["create_firefox_profile"],
     notify  => Service['dev.firefox'],
