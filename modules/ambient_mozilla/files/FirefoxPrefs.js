ad7e6d507589c047d0c432073b710c4d7127be4f
Chris Lonnen <chris.lonnen@gmail.com>
2014-07-24 17:12:17 -0700

    Update FirefoxPrefs.js
    need real booleans, not strings

--- modules/ambient_mozilla/files/FirefoxPrefs.js
+++ modules/ambient_mozilla/files/FirefoxPrefs.js
@@ -1,11 +1,11 @@
 /* A full profile will be created from these preferences. */
 
-user_pref("app.update.auto", "false");
-user_pref("app.update.enabled", "false");
-user_pref("browser.showedAustralisWhatsNew", "true");
+user_pref("app.update.auto", false);
+user_pref("app.update.enabled", false);
+user_pref("browser.showedAustralisWhatsNew", true);
 user_pref("browser.startup.homepage", "http://airmozilla-ops2.corpdmz.scl3.mozilla.com/");
-user_pref("full-screen-api.allow-trusted-requests-only", "false");
-user_pref("full-screen-api.approval-required", "false");
-user_pref("browser.shell.checkDefaultBrowser", "false");
-user_pref("browser.sessionstore.restore_on_demand", "false");
-user_pref("services.sync.prefs.browser.sessionstore.restore_on_demand", "false");
+user_pref("full-screen-api.allow-trusted-requests-only", false);
+user_pref("full-screen-api.approval-required", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.sessionstore.restore_on_demand", false);
+user_pref("services.sync.prefs.browser.sessionstore.restore_on_demand", false);
